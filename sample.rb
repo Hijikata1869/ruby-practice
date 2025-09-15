67897b608c34f44b42322de516af5a8e0888360a
Hijikata1869 <liv.knk@gmail.com>
2025-09-15 12:51:51 +0900

    updated sample.rb

--- sample.rb
+++ sample.rb
@@ -1,7 +1,12 @@
-puts 1 + 2
+point = 7
+day = 1
 
-a = "Hellor, world!"
-puts a
+if day == 1
+  point * 7
+end
 
-b = "こんにちは"
-puts b
+# if文は修飾子として文の後ろに置くこともできる
+# 上のif文とこの修飾子としてのif文は同じ意味
+point *= 5 if day == 1
+
+p point
